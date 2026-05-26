b0c581934e19780d7aa191a353d7ca8d67b3592e
Vijaya101 <vijayabadgujar0@gmail.com>
2026-05-26 12:16:17 +0530

    Updated EduStream project

--- js/script.js
+++ js/script.js
@@ -40,3 +40,49 @@ menuToggle.addEventListener("click", () => {
     navContainer.classList.toggle("show");
 
 });
+// ENROLL POPUP
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    const openFormBtn = document.getElementById("openFormBtn");
+    const closeFormBtn = document.getElementById("closeFormBtn");
+    const popupForm = document.getElementById("popupForm");
+    const enrollForm = document.getElementById("enrollForm");
+
+    if(openFormBtn){
+
+        openFormBtn.addEventListener("click", () => {
+
+            popupForm.style.display = "flex";
+
+        });
+
+    }
+
+    if(closeFormBtn){
+
+        closeFormBtn.addEventListener("click", () => {
+
+            popupForm.style.display = "none";
+
+        });
+
+    }
+
+    if(enrollForm){
+
+        enrollForm.addEventListener("submit", (e) => {
+
+            e.preventDefault();
+
+            alert("Enrollment Successful ✅");
+
+            popupForm.style.display = "none";
+
+            enrollForm.reset();
+
+        });
+
+    }
+
+});
